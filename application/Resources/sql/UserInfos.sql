75177434a5fd9f0a8f8a30ebc534df739c30c324
Takahiro Kotake <tkotake@teleios.jp>
2020-05-23 04:50:57 +0900

    ユーザ情報関連PGアップ

---
+++ application/Resources/sql/UserInfos.sql
@@ -0,0 +1,19 @@
+/************************************
+ * ユーザ情報管理テーブル
+ ************************************/
+DROP TABLE IF EXISTS `UserInfos`;
+CREATE TABLE `UserInfos`
+(
+    `UserId` BIGINT(12) UNSIGNED AUTO_INCREMENT COMMENT '管理ID',
+    `Country` TINYINT(3) UNSIGNED COMMENT '国番号',
+    `State` TINYINT(3) UNSIGNED COMMENT '都道府県',
+    `Gender` TINYINT(1) UNSIGNED COMMENT '性別',
+    `BirthYear` TINYINT(4) UNSIGNED COMMENT '生年(予備)',
+    `BirthMonth` TINYINT(2) UNSIGNED COMMENT '生月(予備)',
+    `BirthDay` TINYINT(2) UNSIGNED COMMENT '生日(予備)',
+    `CreateDate` DATETIME COMMENT 'レコード登録日',
+    `UpdateDate` DATETIME COMMENT 'レコード更新日',
+    `DeleteDate` DATETIME COMMENT 'レコード無効日',
+    `DeleteFlag` TINYINT(1) UNSIGNED DEFAULT 0 COMMENT 'レコード無効フラグ(0:有効, 1:無効)',
+    PRIMARY KEY (`UserId`)
+) ENGINE=InnoDB COMMENT 'ユーザ情報';
---
+++ application/models/dao/UserInfos.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * ユーザ情報管理テーブル操作クラス
+ */
+class UserInfos extends MY_Model
+{
+    const TABLE_NAME = 'UsersInfos';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * レコード追加
+     * @param array $data [description]
+     */
+    public function addNewUserInfos(array $data) : int
+    {
+        return $this->add(self::TABLE_NAME, $data);
+    }
+
+    /**
+     * レコード取得
+     * @param  int $userId [description]
+     * @return int         [description]
+     */
+    public function getUserInfosByUserId(int $userId) : int
+    {
+        $cond = array(
+            'WHERE' => array('UserId' => $userId)
+        );
+        return $this->get(self::TABLE_NAME, $cond);
+    }
+
+    // 修正
+    /**
+     * レコード更新
+     * @param  int   $userId [description]
+     * @param  array $data   [description]
+     * @return bool          [description]
+     */
+    public function updateWithData(int $userId, array $data) : bool
+    {
+        if (count($data) > 0) {
+            return $this->updata(self::TABLE_NAME, $data, array('UserId' => $userId));
+        }
+        return false;
+    }
+
+    // 論理削除
+    public function deleteUserInfos(int $userId) : bool
+    {
+        return $this->delete(self::TABLE_NAME, array('UserId' => $userId));
+    }
+}
